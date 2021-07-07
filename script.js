2451d234082d735e62e904092fb2fca2d22e0483
John Jordan <johnjordan1985@gmail.com>
2021-07-07 08:41:15 -0700

    refactor: into higher order component ahead of adding counter component

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const Clock  = (props) => {
 	const {date} = props; 
+	//const [currentDate, updateDate] = React.useState(date.toLocaleString());
 	return <div>
 		<h1>Hello World</h1>
 		<p>It is now: {date.toLocaleString()}</p>
 	</div>;
 }
 
+const App = () => {
+	return (<>
+		<div id="clock"></div>
+		<div id="counter"></div>
+	</>);
+};
+
 function tick(){
-	ReactDOM.render(<Clock date={new Date()}/>, document.getElementById("root"));
+	ReactDOM.render(<Clock date={new Date()}/>, document.getElementById("clock"));
 }
 
 setInterval(tick, 1000);
 
+ReactDOM.render(<App/>, document.getElementById("root"));
