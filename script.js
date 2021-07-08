ce463570ae900974dee043355ff7109fe92c99e2
John Jordan <johnjordan1985@gmail.com>
2021-07-08 13:49:41 -0700

    feat: add Counter component that increments on click

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const Clock  = (props) => {
 	const {date} = props; 
 	//const [currentDate, updateDate] = React.useState(date.toLocaleString());
-	return <div>
+	return (<div>
 		<h1>Hello World</h1>
 		<p>It is now: {date.toLocaleString()}</p>
-	</div>;
+	</div>);
 }
+
+const Counter = (props) => {
+	const [count, setCount] = React.useState(0);
+	return (<div>
+		<p>Count: {count}</p>
+		<button onClick={() => setCount(count + 1)}>Increment count</button>
+	</div>);
+};
 
 const App = () => {
 	return (<>
 		<div id="clock"></div>
-		<div id="counter"></div>
+		<Counter/>
 	</>);
 };
 
