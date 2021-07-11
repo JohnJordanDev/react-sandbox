8a51c2898a5e71282ef8ede9a5a108bea57398b9
John Jordan <johnjordan1985@gmail.com>
2021-07-11 10:06:50 -0700

    refactor: use Clock component with useEffect, rather than manual DOM updates

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const Clock  = (props) => {
 	const {date} = props; 
-	//const [currentDate, updateDate] = React.useState(date.toLocaleString());
-	return (<div>
-		<h1>Hello World</h1>
-		<p>It is now: {date.toLocaleString()}</p>
-	</div>);
+	const [currentDate, updateDate] = React.useState(date.toLocaleString());
+	React.useEffect(() => {
+		window.setTimeout(()=>{
+			updateDate(window.Date().toLocaleString());
+		},1000);
+	});
+	return (
+		<div>
+			<h1>Hello World</h1>
+			<p>It is now: {currentDate}</p>
+		</div>
+	);
 }
 
 const Counter = (props) => {
 	const {initialCount} = props;
 	const [count, setCount] = React.useState(initialCount);
 	React.useEffect(()=>{
-		//console.log(`just logged, for a count of ${count}`);
 		const timerId = setInterval(()=>{
 			console.log(`logging, from when a count of ${count}`);
 		}, 2000);
@@ -21,23 +27,19 @@ const Counter = (props) => {
 			clearInterval(timerId);
 		};
 	});
-	return (<div>
-		<p>Count: {count}</p>
-		<button onClick={() => setCount(count + 1)}>Increment count</button>
-	</div>);
+	return (
+		<div>
+			<p>Count: {count}</p>
+			<button onClick={() => setCount(count + 1)}>Increment count</button>
+		</div>
+	);
 };
 
 const App = () => {
 	return (<>
-		<div id="clock"></div>
+		<Clock date={Date()}/>
 		<Counter initialCount={2}/>
 	</>);
 };
-
-function tick(){
-	ReactDOM.render(<Clock date={new Date()}/>, document.getElementById("clock"));
-}
-
-setInterval(tick, 1000);
 
 ReactDOM.render(<App/>, document.getElementById("root"));
